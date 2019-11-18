b7878c3084d1812ac30cd3b67591f97b4c325b8b
lamborghini1993 <1323242382@qq.com>
2019-11-18 20:25:04 +0800

    shader

--- 1-5-Practice-2.cpp
+++ 1-5-Practice-2.cpp
@@ -113,7 +113,7 @@ int main()
     CreateTexture(&texture1, "Textures/container.jpg", GL_RGB, GL_CLAMP_TO_EDGE);
     CreateTexture(&texture2, "Textures/awesomeface.png", GL_RGBA, GL_REPEAT, true);
 
-    Shader shader("Shaders/1-5-textures-2.vs", "Shaders/1-5-textures-2.fs");
+    Shader shader("Shaders/1-5-practice-2.vs", "Shaders/1-5-practice-2.fs");
     shader.use();
     shader.setInt("ourTexture1", 0);
     shader.setInt("ourTexture2", 1);
