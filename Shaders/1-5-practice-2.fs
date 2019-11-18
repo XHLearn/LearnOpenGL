36dd015cf666ab584966a353c76b08b3cd60db34
lamborghini1993 <1323242382@qq.com>
2019-11-18 20:15:50 +0800

    1-5-practice-2 done
    在箱子的角落放置4个笑脸

--- Shaders/1-5-practice-2.fs
+++ Shaders/1-5-practice-2.fs
@@ -10,6 +10,5 @@ uniform sampler2D ourTexture2;
 void main()
 {
     // linearly interpolate between both textures (80% container, 20% awesomeface)
-    vec2 reface = vec2(1-TexCoord.x, TexCoord.y);
-    FragColor = mix(texture(ourTexture1,TexCoord),texture(ourTexture2,reface),0.2);
+    FragColor = mix(texture(ourTexture1,TexCoord),texture(ourTexture2,TexCoord),0.2);
 }
