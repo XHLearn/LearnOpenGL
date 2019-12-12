e7836fbd35ae3ceef5ed700378448a39907e31e4
lamborghini1993 <1323242382@qq.com>
2019-12-12 15:42:15 +0800

    优化

--- 2-6-Practice.cpp
+++ 2-6-Practice.cpp
@@ -212,7 +212,6 @@ int main()
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-    glEnableVertexAttribArray(0);
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
     glEnableVertexAttribArray(2);
 
