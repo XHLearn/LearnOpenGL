8a6b88229374585125ddd3e28bff031256f275f0
lamborghini1993 <1323242382@qq.com>
2019-12-23 20:44:46 +0800

    Framebuffers

--- src/4-AdvancedOpenGL/5-Framebuffers/Framebuffers.cpp
+++ src/4-AdvancedOpenGL/5-Framebuffers/Framebuffers.cpp
@@ -180,9 +180,16 @@ int main()
         // positions       // texture Coords (note we set these higher than 1 (together with GL_REPEAT as texture wrapping mode). this will cause the floor texture to repeat)
     };
 
-    unsigned int cubeTexture, floorTexture;
-    cubeTexture = loadTexture("resources/textures/marble.jpg");
-    floorTexture = loadTexture("resources/textures/metal.png");
+    float quadVertices[] = {
+        // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
+        // positions   // texCoords
+        -1.0f, 1.0f, 0.0f, 1.0f,
+        -1.0f, -1.0f, 0.0f, 0.0f,
+        1.0f, -1.0f, 1.0f, 0.0f,
+
+        -1.0f, 1.0f, 0.0f, 1.0f,
+        1.0f, -1.0f, 1.0f, 0.0f,
+        1.0f, 1.0f, 1.0f, 1.0f};
 
     // cube VAO
     unsigned int cubeVAO, cubeVBO;
@@ -195,10 +202,6 @@ int main()
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
-    glBindVertexArray(0);
-    // TODO 先绑定贴图有问题
-    // glActiveTexture(GL_TEXTURE);
-    // glBindTexture(GL_TEXTURE_2D, cubeTexture);
 
     // plane VAO
     unsigned int planeVAO, planeVBO;
@@ -211,20 +214,65 @@ int main()
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
-    glBindVertexArray(0);
-    // glActiveTexture(GL_TEXTURE);
-    // glBindTexture(GL_TEXTURE_2D, floorTexture);
 
-    Shader shader("src/4-AdvancedOpenGL/5-Framebuffers/Framebuffers.vs", "src/4-AdvancedOpenGL/5-Framebuffers/Framebuffers.fs");
-    shader.use();
-    shader.setInt("texture1", 0);
+    // screen quad VAO
+    unsigned int quadVAO, quadVBO;
+    glGenVertexArrays(1, &quadVAO);
+    glGenBuffers(1, &quadVBO);
+    glBindVertexArray(quadVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)(2 * sizeof(float)));
 
+    unsigned int cubeTexture, floorTexture;
+    cubeTexture = loadTexture("resources/textures/container.jpg");
+    floorTexture = loadTexture("resources/textures/metal.png");
+    Shader cubeShader("src/4-AdvancedOpenGL/5-Framebuffers/Depthtesting.vs", "src/4-AdvancedOpenGL/5-Framebuffers/Depthtesting.fs");
+    Shader screenShader("src/4-AdvancedOpenGL/5-Framebuffers/Framebuffers.vs", "src/4-AdvancedOpenGL/5-Framebuffers/Framebuffers.fs");
+    cubeShader.use();
+    cubeShader.setInt("texture1", 0);
+    screenShader.use();
+    screenShader.setInt("screenTexture", 0);
+
+    unsigned int framebuffer;
+    glGenFramebuffers(1, &framebuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+
+    // 给帧缓冲附加一个纹理附件
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    // 将它附加到当前绑定的帧缓冲对象
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture, 0);
+
+    unsigned int rbo;
+    glGenRenderbuffers(1, &rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, SCR_WIDTH, SCR_HEIGHT);
+    glBindRenderbuffer(GL_RENDERBUFFER, 0);
+    // 将渲染缓冲对象附加到帧缓冲的深度和模板附件
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    {
+        cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << endl;
+        return -1;
+    }
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    // 鼠标键盘事件
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
 
     glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_ALWAYS); // 永远通过深度测试
 
     while (!glfwWindowShouldClose(window)) // 让GLFW退出前一直保持运行
     {
@@ -236,6 +284,10 @@ int main()
         glfwPollEvents();        // glfwPollEvents函数检查有没有触发什么事件（比如键盘输入、鼠标移动等），然后调用对应的回调函数
         glfwSwapBuffers(window); // glfwSwapBuffers函数会交换颜色缓冲（它是一个储存着GLFW窗口每一个像素颜色的大缓冲）
 
+        // 第一处理阶段(Pass)
+        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+        glEnable(GL_DEPTH_TEST);
+
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -243,33 +295,48 @@ int main()
         glm::mat4 view = cam.GetViewMatrix();
         glm::mat4 projection = glm::perspective(glm::radians(cam.Zoom), float(SCR_WIDTH) / float(SCR_HEIGHT), 0.1f, 100.0f);
 
-        // shader
-        shader.use();
-        shader.setMat4("view", view);
-        shader.setMat4("projection", projection);
+        // cubeShader
+        cubeShader.use();
+        cubeShader.setMat4("view", view);
+        cubeShader.setMat4("projection", projection);
 
         // cubes
         glBindVertexArray(cubeVAO);
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, cubeTexture);
         model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
-        shader.setMat4("model", model);
+        cubeShader.setMat4("model", model);
         glDrawArrays(GL_TRIANGLES, 0, 36);
         model = glm::mat4(1.0f);
         model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
-        shader.setMat4("model", model);
+        cubeShader.setMat4("model", model);
         glDrawArrays(GL_TRIANGLES, 0, 36);
         // floor
         glBindVertexArray(planeVAO);
         glBindTexture(GL_TEXTURE_2D, floorTexture);
-        shader.setMat4("model", glm::mat4(1.0f));
+        cubeShader.setMat4("model", glm::mat4(1.0f));
         glDrawArrays(GL_TRIANGLES, 0, 6);
         glBindVertexArray(0);
+
+        // 第二处理阶段
+        glBindFramebuffer(GL_FRAMEBUFFER, 0); // 返回默认
+        glDisable(GL_DEPTH_TEST);
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        screenShader.use();
+        glBindVertexArray(quadVAO);
+        glDisable(GL_DEPTH_TEST);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
     }
     glDeleteVertexArrays(1, &cubeVAO);
     glDeleteVertexArrays(1, &planeVAO);
+    glDeleteVertexArrays(1, &quadVAO);
     glDeleteBuffers(1, &cubeVBO);
     glDeleteBuffers(1, &planeVBO);
+    glDeleteBuffers(1, &quadVBO);
+    glDeleteBuffers(1, &framebuffer);
 
     glfwTerminate(); // glfwTerminate函数来释放GLFW分配的内存
     return 0;
--- src/4-AdvancedOpenGL/5-Framebuffers/Framebuffers.fs
+++ src/4-AdvancedOpenGL/5-Framebuffers/Framebuffers.fs
@@ -3,20 +3,9 @@ out vec4 FragColor;
 
 in vec2 TexCoords;
 
-uniform sampler2D texture1;
-
-float near = 0.1; 
-float far  = 100.0; 
-
-float LinearizeDepth(float depth)
-{
-    float z = depth * 2.0 - 1.0;
-    return (2.0 * near * far) / (far + near - z * (far + near));
-}
+uniform sampler2D screenTexture;
 
 void main()
 {    
-    // FragColor = texture(texture1, TexCoords);
-    float depth = LinearizeDepth(gl_FragCoord.z) / far; 
-    FragColor = vec4(vec3(depth), 1.0);
+    FragColor = texture(screenTexture, TexCoords);
 }
